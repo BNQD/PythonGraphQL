f558d9eaab000550519edb5f3ebbc4860f4b4a4c
BOBO-PC\namqu <nam.quang.doan@gmail.com>
2025-03-20 09:58:58 -0400

    First post

--- myapp/api/__init__.py
+++ myapp/api/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
 
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://localhost/GraphQL?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
